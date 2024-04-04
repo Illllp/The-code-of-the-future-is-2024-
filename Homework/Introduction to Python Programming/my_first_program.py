4e982944dc48212f0f7696df19a888591dff0b63
Родион Шиловский <lukasjohancen@yandex.ru>
2024-04-04 21:35:01 +0300

    Record and print user's full name

---
+++ Homework/Introduction to Python Programming/my_first_program.py
@@ -0,0 +1,7 @@
+# Recording the full user name
+my_full_name = "Shilovsky Rodion Mikhailovich"
+
+# Printing a greeting message with the user's full name
+print(f'Hello, {my_full_name}, nice to meet you.')
+
+
