b3f259af4d2f0b209c44d67723fc3693385c43bd
Родион Шиловский <lukasjohancen@yandex.ru>
2024-05-26 01:23:50 +0300

    From 0 to n

---
+++ Homework/While and For loops/Basic level/Code using a while loop.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter an integer
+n = int(input("Enter an integer: "))
+
+# Use a while loop to print numbers from 0 to n (inclusive)
+i = 0
+while i <= n:
+  print(i)
+  i += 1
