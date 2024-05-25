0a6daaf574781a1e331496492ea2aa1d41ba0c02
Родион Шиловский <lukasjohancen@yandex.ru>
2024-05-26 01:03:07 +0300

    Working with lists and slices

---
+++ Homework/Lists/Entry level/Task 1: Working with lists and slices.py
@@ -0,0 +1,20 @@
+# Create a list with different data types
+data_list = ["String", 10, True, 5.5, "Another string", False, 20, "Number 3", 0.1, None]
+
+# Print the entire list
+print(data_list)
+
+# Print the first 5 elements
+print(data_list[:5])
+
+# Print the last 3 elements
+print(data_list[-3:])
+
+# Print every other element
+print(data_list[::2])
+
+# Modify the 3rd element
+data_list[2] = "New element"
+
+# Print the modified list
+print(data_list)
