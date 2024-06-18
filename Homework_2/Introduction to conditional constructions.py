e82f0f6ed5a13e7ff6872d3396b3c172dbc5a309
Родион Шиловский <lukasjohancen@yandex.ru>
2024-06-18 19:09:53 +0300

    Working with lists

---
+++ Homework_2/Introduction to conditional constructions.py
@@ -0,0 +1,25 @@
+'''A program for working with a dictionary'''
+
+#Creating a dictionary
+
+car = {
+        'Make': 'WW',
+        'model': 'WY',
+        'year': '1992',
+        }
+#Adding a color with the meaning black to the dictionary
+car['color']='black'
+
+#deducing the mark value from the dictionary
+print(car['Make'])
+print(car)
+
+#list fruits
+fruits = ["apple", "banana", "orange", "grape", "strawberry"]
+
+print(fruits[0])
+
+#dictionary numbers
+
+numbers = (1,2,3,4,5,666)
+print(numbes[-1])
