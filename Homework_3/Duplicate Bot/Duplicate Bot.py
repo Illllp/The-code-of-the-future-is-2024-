58eb598b6f6a19df2bd2a5985cefcffceb90eaad
Родион Шиловский <lukasjohancen@yandex.ru>
2024-07-31 23:32:17 +0300

    Duplicate Bot

--- Homework_3/Duplicate Bot/Duplicate Bot.py
+++ Homework_3/Duplicate Bot/Duplicate Bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("7295010578:AAFUpzLQ0bnzFM3CabrJAICmEcEsQ4I8QiQ") #Токен
+bot = telebot.TeleBot("") #Токен
 
 @bot.message_handler(content_types=['text'])
 def repeat_message(message):
