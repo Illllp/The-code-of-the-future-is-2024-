7088c004a391f698a20410354272061a1dfbe1bb
Шиловский Родион Михайлович <lukasjohancen@yandex.ru>
2024-08-20 16:27:14 +0300

    Adding pages to connect to the template

--- Homework_4/Temlates_Django_base/mod4/temlates_base/views.py
+++ Homework_4/Temlates_Django_base/mod4/temlates_base/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'base/base.html')
+
+
+def page1(request):
+    return render(request, 'page/page_1.html')
+
+
+def page2(request):
+    return render(request, 'page/page_2.html')
+
+
+def page3(request):
+    return render(request, 'page/page_3.html')
+
+
+
